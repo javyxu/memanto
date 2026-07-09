9138b76f18b38c5ed2d7b3fab81d0c91e6d3373f
smwsk1 <austinclaire428@gmail.com>
2026-07-09 17:13:29 +0800

    Add relative time helper regression tests

--- tests/test_temporal_helpers.py
+++ tests/test_temporal_helpers.py
@@ -1,6 +1,6 @@
 from datetime import timezone
 
-from memanto.app.utils.temporal_helpers import parse_iso_timestamp
+from memanto.app.utils.temporal_helpers import parse_iso_timestamp, parse_relative_time
 
 
 def test_parse_iso_timestamp_normalizes_offset_to_utc():
@@ -15,3 +15,10 @@ def test_parse_iso_timestamp_assumes_naive_values_are_utc():
 
     assert parsed.tzinfo == timezone.utc
     assert parsed.isoformat() == "2026-01-15T13:30:00+00:00"
+
+
+def test_parse_relative_time_rejects_non_positive_windows():
+    assert parse_relative_time("last 0 days") is None
+    assert parse_relative_time("last -1 days") is None
+    assert parse_relative_time("last 0 hours") is None
+    assert parse_relative_time("last -2 hours") is None
